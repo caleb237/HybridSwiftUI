fcee245ae71d072a183ce861fa91d8c398ad2172
HW-11135 <choisk@hanwha.com>
2022-11-26 23:48:18 +0900

    최초커밋
    최초커밋

---
+++ HybridSwiftUI/HybridSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HybridSwiftUIApp.swift
+//  HybridSwiftUI
+//
+//  Created by HW-11135 on 2022/11/26.
+//
+
+import SwiftUI
+
+@main
+struct HybridSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
